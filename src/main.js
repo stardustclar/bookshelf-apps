22faa61014c1fba3f2d29d6b2588edd8f51ec937
stardustclar <claritadewi221@gmail.com>
2023-11-30 15:09:38 +0700

    edit search

--- src/main.js
+++ src/main.js
@@ -1,36 +1,31 @@
 // app.js
 const unfinishedBooksShelf = document.getElementById("unfinished-books");
 const finishedBooksShelf = document.getElementById("finished-books");
-const unfinishedBookList = document.getElementById("unfinished-book-list");
-const finishedBookList = document.getElementById("finished-book-list");
+const unfinishedBookList = document.getElementById('unfinished-book-list');
+const finishedBookList = document.getElementById('finished-book-list');
 const addBookForm = document.getElementById("add-book-form");
-const searchInput = document.getElementById("searchBookTitle");
-const searchButton = document.getElementById("searchSubmit");
 
 let books = [];
 
-function searchBooks() {
-  const searchInput = document.getElementById("searchBookTitle");
-  const searchTerm = searchInput.value.toLowerCase();
-  const bookList = document.getElementById("bookList");
-  bookList.innerHTML = "";
+document.getElementById('searchSubmit').addEventListener("click", 
+function (event) {
+  const searchBook =
+  document.getElementById('searchBookTitle').value.toLowerCase()
+  const bookList = document.querySelectorAll('.book-item > h3')
 
-  const matchingBooks = books.filter((book) =>
-    book.title.toLowerCase().includes(searchTerm)
-  );
-
-  matchingBooks.forEach((book) => {
-    const li = document.createElement("li");
-    li.textContent = `${book.title} - ${book.writer}`;
-    bookList.appendChild(li);
-  });
-}
+  for (books of bookList) {
+    if (searchBook  === bookList[0].innerText.toLowerCase()) {
+      books.parentElement.style.display = "block";
+      } else {
+      }
+    }   
+  })
 
 function loadBooksFromLocalStorage() {
-  const storedBooks = localStorage.getItem("books");
+  const storedBooks = localStorage.getItem('books')
   if (storedBooks) {
-    books = JSON.parse(storedBooks);
-    renderBooks();
+    books = JSON.parse(storedBooks)
+    renderBooks()
   }
 }
 
@@ -47,7 +42,7 @@ addBookForm.addEventListener("submit", (event) => {
     title: titleInput.value,
     writer: writerInput.value,
     year: parseInt(yearInput.value),
-    isComplete: isCompleteInput,
+    isComplete: isCompleteInput
   };
 
   books.push(newBook);
@@ -60,6 +55,7 @@ addBookForm.addEventListener("submit", (event) => {
   isCompleteInput.value = "";
 });
 
+
 function renderBooks() {
   const unfinishedBooksShelf = document.getElementById("unfinished-books");
   const finishedBooksShelf = document.getElementById("finished-books");
@@ -67,48 +63,45 @@ function renderBooks() {
     console.error("One or more bookshelves not found!");
     return;
   }
-
+  
   unfinishedBooksShelf.innerHTML = "";
   finishedBooksShelf.innerHTML = "";
 
   books.forEach((book) => {
+    
     const bookItem = document.createElement("article");
-    bookItem.classList.add("book-item");
+    bookItem.classList.add('book-item'); 
 
-    const bookInfo = document.createElement("div");
-    const bookTitle = document.createElement("h3");
+    const bookInfo = document.createElement('div');
+    const bookTitle = document.createElement('h3');
     bookTitle.textContent = book.title;
-    const bookWriter = document.createElement("p");
+    const bookWriter = document.createElement('p');
     bookWriter.textContent = `Writer: ${book.writer}`;
-    const bookYear = document.createElement("p");
+    const bookYear = document.createElement('p');
     bookYear.textContent = `Year: ${book.year}`;
 
     bookInfo.appendChild(bookTitle);
     bookInfo.appendChild(bookWriter);
     bookInfo.appendChild(bookYear);
 
-    const deleteButton = document.createElement("button");
-    deleteButton.textContent = "Delete";
-    deleteButton.addEventListener("click", () => {
-      const confirm = window.confirm(
-        "Are you sure you want to delete this book?"
-      );
-
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = 'Delete';
+    deleteButton.addEventListener('click', () => {
+      const confirm = window.confirm('Are you sure you want to delete this book?');
+      
       if (confirm) {
         deleteBook(book.id);
       }
     });
 
-    const moveButton = document.createElement("button");
-    moveButton.textContent = book.isComplete
-      ? "Move to Unfinished"
-      : "Move to Finished";
-    moveButton.addEventListener("click", () => {
+    const moveButton = document.createElement('button');
+    moveButton.textContent = book.isComplete ? 'Move to Unfinished' : 'Move to Finished';
+    moveButton.addEventListener('click', () => {
       moveBook(book.id);
     });
 
-    deleteButton.classList.add("red");
-    moveButton.classList.add("green");
+    deleteButton.classList.add('red');
+    moveButton.classList.add('green');
 
     bookItem.appendChild(bookInfo);
     bookItem.appendChild(deleteButton);
@@ -142,13 +135,24 @@ function updateLocalStorage() {
 }
 
 function loadBooksFromLocalStorage() {
-  const storedBooks = localStorage.getItem("books");
+  const storedBooks = localStorage.getItem('books')
   if (storedBooks) {
-    books = JSON.parse(storedBooks);
-    renderBooks();
+    books = JSON.parse(storedBooks)
+    renderBooks()
   }
 }
 
-loadBooksFromLocalStorage();
+loadBooksFromLocalStorage()
 
 
+// function editBook(bookId) {
+//   const bookIndex = books.findIndex((book) => book.id === bookId);
+//   if (bookIndex !== -1) {
+//     const bookTitleInput = document.getElementById("bookTitle");
+//     const bookwriterInput = document.getElementById("bookwriter");
+//     const bookYearInput = document.getElementById("bookYear");
+//     bookTitleInput.value = books[bookIndex].title;
+//     bookwriterInput.value = books[bookIndex].writer;
+//     bookYearInput.value = books[bookIndex].year;
+//   }
+// }
